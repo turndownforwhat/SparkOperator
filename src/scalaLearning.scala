832d0e73bcb10c0ed6298ffaa6bb94a1bf38c2d1
turndownforwhat <285582164@qq.com>
2018-05-28 00:50:05 +0800

    append timestamp

--- src/scalaLearning.scala
+++ src/scalaLearning.scala
@@ -4,13 +4,13 @@ object scalaLearning {
   def main(args:Array[String]): Unit = {
     val gson = new Gson
     val httpUtil = new HttpUtil
-    var content = httpUtil.getRestContent("http://10.8.0.32:9090/contexts/123")
+    var content = httpUtil.getRestContent("http://10.8.0.32:9090/contexts/haoyucontext")
 
     //val jsoncon = gson.toJson(content)
 
     var returnData = new JsonParser().parse(content).getAsJsonObject
 
-    println(returnData.get("result"))
+    println(returnData.get("flow_id"))
 
 
 
