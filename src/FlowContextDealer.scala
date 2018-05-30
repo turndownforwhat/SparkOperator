9db6b61bda626bec6eaf9eb9cc92b18b9a1dc383
hiros <285582164@qq.com>
2018-05-30 11:12:09 +0800

    add some comment

--- src/FlowContextDealer.scala
+++ src/FlowContextDealer.scala
@@ -1,13 +1,16 @@
 import com.google.gson._
 
 class FlowContextDealer {
+  // 用于存放工作流上下文
   var context = new JsonObject
 
+  // 通过传入的Json对象进行实例构造
   def this(jsonObject: JsonObject){
     this()
     this.context = jsonObject
   }
 
+  // 根据操作节点id读取其前置节点
   def readOperatorPres(operatorId:String):JsonArray = {
     var dbInfoJsonArray = new JsonArray
     var preIds = this.readOperator(operatorId)
@@ -31,6 +34,7 @@ class FlowContextDealer {
     dbInfoJsonArray
   }
 
+  // 根据操作节点id读取节点内信息
   def readOperator(operatorId:String):JsonObject = {
     context.getAsJsonObject("context")
       .getAsJsonObject("body")
@@ -38,9 +42,15 @@ class FlowContextDealer {
       .getAsJsonObject(operatorId)
   }
 
+  // 根据操作节点id读取节点内输出内容
   def readOperatorOutputs(operatorId:String):JsonArray = {
     this.readOperator(operatorId)
       .getAsJsonArray("outputs")
+  }
+
+  // 根据操作节点id更新该节点的结果输出内容
+  def updateOperatorOutput(operatorId:String,resultSize:Int):JsonArray = {
+
   }
 
 
